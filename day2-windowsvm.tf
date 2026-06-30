e581173a0a888fcecafc9fcb32de53b3e55e9c5c
gopal1409 <gopal1409@gmail.com>
2026-06-30 15:52:59 +0530

    Integrate Azure Key Vault to retrieve VM password
    Added data blocks to call an existing Azure Key Vault and retrieve a secret.

--- day2-windowsvm.tf
+++ day2-windowsvm.tf
@@ -9,3 +9,19 @@
   #then expand object 
   #click on secret give a click on generate import
   #give a name and a value which will be your password Admin@12345678
+#lets call the vault create a new file a13.vault.tf
+#in this data block we are calling the vault
+data "azurerm_key_vault" "existing_kv" {
+  name                = "gopal-vault" #change it to your name of the vault 
+  resource_group_name = "gopal-win-vault" #change it to your resource group name of the vault
+}
+
+output "vault_uri" {
+  value = data.azurerm_key_vault.existing_kv.vault_uri
+}
+
+#second we need to call the secret from the vault
+data "azurerm_key_vault_secret" "vm_password" {
+    name         = "win-pass" #change it to secret name created inside the vault
+    key_vault_id = data.azurerm_key_vault.existing_kv.id
+}
