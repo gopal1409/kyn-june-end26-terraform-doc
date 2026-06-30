4d0d71e2a8c63476bedabbaa406f1f20b96b3a68
gopal1409 <gopal1409@gmail.com>
2026-06-30 15:35:51 +0530

    Add setup instructions for Windows VM and Key Vault
    Added instructions for disabling the Linux VM and creating a Key Vault.

---
+++ day2-windowsvm.tf
@@ -0,0 +1,11 @@
+#lets disable the linux vm 
+#before that run terraform destroy
+#then go to a11.linuxvm.tf in the starting put /* and end of the line */
+  #then go to azure portal search for Key vaults
+  #create a new one 
+  #create a new resource group gopal-win-vault 
+  #give a name to your vault gopal-vault
+  ###then create it
+  #then expand object 
+  #click on secret give a click on generate import
+  #give a name and a value which will be your password Admin@12345678
